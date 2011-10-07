6f6b23a45de15c9d3b9deb187992d24359b0b954
Ângelo Otávio Nuffer Nunes <angelonuffer@gmail.com>
2011-10-07 17:59:35 -0300

    improve login form layout

--- lib/widget.rb
+++ lib/widget.rb
@@ -12,37 +12,39 @@ class Widget
     @element
   end
   def put
-    @ws.send "document.write('%s')" % @element.to_s
+    @ws.send "document.body.innerHTML += '%s'" % @element.to_s
   end
 end
 
 class LoginForm < Widget
   def initialize(ws)
     element = Element.new "div"
+    title = Element.new "h2"
+    title.append "Login"
     name = Field.new(ws, "name")
-    password = Field.new(ws, "password")
-    signup = Element.new "a"
-    signup[:href] = "#"
+    password = Field.new(ws, "password", "password")
+    signup = Element.new "button"
     signup[:onclick] = "sock.send(\\'login_form signup\\')"
     signup.append "sign up"
     login = Element.new "button"
     login[:onclick] = "sock.send(\\'login_form login\\')"
     login.append "log in"
+    element.append title
     element.append name
     element.append password
-    element.append signup
     element.append login
+    element.append signup
     super(ws, element, "login_form")
   end
 end
 
 class Field < Widget
-  def initialize(ws, name)
+  def initialize(ws, name, type="text")
     element = Element.new "div"
     label = Element.new "p"
     label.append "%s:" % name
     input = Element.new "input"
-    input[:type] = "text"
+    input[:type] = type
     input[:name] = name.gsub(" ", "_")
     element.append label
     element.append input
--- spec/widget_spec.rb
+++ spec/widget_spec.rb
@@ -3,7 +3,7 @@ require './lib/widget.rb'
 describe Widget do
   it "has an element" do
     ws = mock()
-    ws.stub!(:send).with("document.write('<div id=\"widget_id\" />')")
+    ws.stub!(:send).with("document.body.innerHTML += '<div id=\"widget_id\" />'")
     div = Element.new "div"
     widget = Widget.new(ws, div, "widget_id")
     widget.element.should == div
@@ -18,7 +18,7 @@ describe LoginForm do
     ws.stub!(:send)
     login_form = LoginForm.new ws
     login_form.element.to_s.should include '<input type="text" name="name" />'
-    login_form.element.to_s.should include '<input type="text" name="password" />'
+    login_form.element.to_s.should include '<input type="password" name="password" />'
   end
   it "has a button to log in" do
     ws = mock()
@@ -26,11 +26,11 @@ describe LoginForm do
     login_form = LoginForm.new ws
     login_form.element.to_s.should include '<button onclick="sock.send(\\\'login_form login\\\')">log in</button>'
   end
-  it "has a link to sign up" do
+  it "has a button to sign up" do
     ws = mock()
     ws.stub!(:send)
     login_form = LoginForm.new ws
-    login_form.element.to_s.should include '<a href="#" onclick="sock.send(\\\'login_form signup\\\')">sign up</a>'
+    login_form.element.to_s.should include '<button onclick="sock.send(\\\'login_form signup\\\')">sign up</button>'
   end
 end
 
